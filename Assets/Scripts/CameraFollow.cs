f06a2e909f56fbeffb682fc7dbcfc4eb6050fba7
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-10 21:01:30 +0300

    commit x
    commit x

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    public float smoothSpeed = 0.04f;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = Vector3.Lerp(transform.position, target.position + offset, smoothSpeed);
+
+        transform.position = newPos;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody playerRB;
-    public float bounceForce = 6f;
+    public Rigidbody playerRB; // Player RigidBody
+    public float bounceForce = 6f; // Player Bounce Force
 
+    // When player collides with ring => Bounce player
     private void OnCollisionEnter(Collision collision)
     {
         playerRB.velocity = new Vector3(playerRB.velocity.x, bounceForce, playerRB.velocity.z);
