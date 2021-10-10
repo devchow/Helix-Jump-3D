d6269a45edf67dd1cb74a9fbb4dba0371fb92d1b
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-10 20:27:54 +0300

    commit x
    commit x

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 150f;
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X");
+
+            transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+        }
+    }
+}
