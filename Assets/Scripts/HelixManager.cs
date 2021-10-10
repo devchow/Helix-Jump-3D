fe432d8a0459517dbb98de59d623d7fb3b3a8066
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-10 22:58:48 +0300

    commit x

--- Assets/Scripts/HelixManager.cs
+++ Assets/Scripts/HelixManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class HelixManager : MonoBehaviour
 {
-    void Start()
+    public GameObject[] helixRings;
+    public float ySpawn = 0;
+    public float ringDistance = 5f;
+
+    public int numberOfRings = 7;
+
+    private void Start()
     {
-        
+        // Spawning the Helix + Rings
+        for(int i = 0; i < numberOfRings; i++)
+        {
+            SpawnRing(Random.Range(0, helixRings.Length));
+        }
     }
 
-    void Update()
+    public void SpawnRing(int ringIndex)
     {
-        
+        GameObject go = Instantiate(helixRings[ringIndex], transform.up * ySpawn, Quaternion.identity);
+        go.transform.parent = transform;
+        ySpawn -= 5;
     }
 }
