d44895705a0b3c4492aa1610c8c3e357c6a3e559
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-12 00:53:13 +0300

    commit x
    commit x

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject[] characters;
+    private int selectCharacters;
+
+    void Start()
+    {
+        foreach(GameObject ch in characters)
+        {
+            ch.SetActive(false);
+        }
+        characters[selectCharacters].SetActive(true);
+    }
+
+}
